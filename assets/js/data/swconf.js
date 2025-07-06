c6df3fbba668e366b29be8f10be7646af249fa1d
jwater000 <jwater000@users.noreply.github.com>
2025-07-06 10:37:00 +0000

    deploy: 638f2d056ca007868803071bee5c755994ed6ddf

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1751798220',resources: [
+      '/assets/css/.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
